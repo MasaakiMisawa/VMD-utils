64df972e39cf1cac79887e6bb5e9539eb47409c1
Masaaki Misawa <masaaki.misawa@gmail.com>
2016-01-08 14:36:16 -0800

    add function [readeigv2]

--- misawa.tcl
+++ misawa.tcl
@@ -219,6 +219,7 @@ set pset on
 ##  Memo:                                                                ##
 ##   Lattice parameters are required                                     ##
 ##   Only for orthorhombic cell                                          ##
+##   New format: readeigv2 (2016.1.8)                                    ##
 ##                                                                       ##
 ###########################################################################
 
@@ -964,6 +965,7 @@ proc readeigv {args} {
     lappend zVec 0 
     lappend zVec [molinfo top get c]
     
+#    set nn 0
     puts "start readig $filename($ii)"  
     for {set j 0} {$j < $ngrid} {incr j} {
       set n [gets $rfile]
@@ -973,9 +975,84 @@ proc readeigv {args} {
 #        lappend valList $eval
       }
       incr j [expr $n - 1]
+      set nn [expr $nn + $n]
+    }
+
+#    puts "ngrid = $ngrid value = $nn"
+    puts "end reading $filename($ii)"
+    close $rfile
+    if {[lindex $args [expr $ii + 1]] == "as"} then {
+      set vdnam [lindex $args [expr $ii + 2]]
+      incr ii 2
+    } else {
+      set vdnam $filename($ii)
+    }
+    mol volume $molid $vdnam $vorigin $xVec $yVec $zVec [lindex $vgrid 0] [lindex $vgrid 1] [lindex $vgrid 2] $valList
+    # mol volume $molid $filename($ii) $vorigin $xVec $yVec $zVec [lindex $vgrid 0] [lindex $vgrid 1] [lindex $vgrid 2] [vecscale $evfact $valList]
+    puts "accepted as $vdnam"
+  }
+}
+
+###################################################################
+
+puts "readeigv2"
+proc readeigv2 {args} {
+  set molid top
+  set nargs [llength $args]
+
+  for {set ii 0} {$ii < $nargs} {incr ii} {
+    set filename($ii) [lindex $args $ii]
+    set rfile [open $filename($ii) r]
+    set dummy [gets $rfile]
+    if {[lindex $dummy 0] == 0} then {
+    puts "Use readeigv!"
+    }
+    set ovorigin [gets $rfile]
+    set ovgrid [gets $rfile]
+    set shorigin [gets $rfile]
+    set evfact [gets $rfile]
+    set valList ""
+    
+    set lgx [lindex $shorigin 3]
+    set lgy [lindex $shorigin 4]
+    set lgz [lindex $shorigin 5]
+
+    puts "$lgx $lgy $lgz"
+
+    set vorigin [expr [lindex $shorigin 0]*[molinfo top get a]/[lindex $ovgrid 0]]
+    lappend vorigin [expr [lindex $shorigin 1]*[molinfo top get b]/[lindex $ovgrid 1]]
+    lappend vorigin [expr [lindex $shorigin 2]*[molinfo top get c]/[lindex $ovgrid 2]]
+
+    set xVec [expr $lgx*[molinfo top get a]]
+    lappend xVec 0
+    lappend xVec 0
+    set yVec 0
+    lappend yVec [expr $lgy*[molinfo top get b]]
+    lappend yVec 0
+    set zVec 0
+    lappend zVec 0
+    lappend zVec [expr $lgz*[molinfo top get c]]
+
+    set vgrid $lgx
+    lappend vgrid $lgy
+    lappend vgrid $lgz
+    set ngrid [expr $lgx*$lgy*$lgz]
+#    set nn 0
+
+    puts "start reading $filename($ii)"
+    for {set j 0} {$j < $ngrid} {incr j} {
+      set n [gets $rfile]
+      set eval [gets $rfile]
+      for {set i 0} {$i < $n} {incr i} {
+        lappend valList [expr $eval*$evfact]
+#        lappend valList $eval
+      }
+      incr j [expr $n - 1]
+#      set nn [expr $nn + $n]
     }
 
     puts "end reading $filename($ii)"
+#    puts "ngrid = $ngrid value = $nn"
     close $rfile
     if {[lindex $args [expr $ii + 1]] == "as"} then {
       set vdnam [lindex $args [expr $ii + 2]]
